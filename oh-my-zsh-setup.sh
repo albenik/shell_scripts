cc9cfdfe2cbf8c992a24bbc976562c6720ea0f4b
albenik <albenik@gmail.com>
2015-03-19 14:59:35 +0300

    Added oh-my-zsh install line

--- oh-my-zsh-setup.sh
+++ oh-my-zsh-setup.sh
@@ -3,6 +3,8 @@ OMZ_CUSTOM_DIR="$OMZ_DIR/custom"
 OMZ_THEME_DIR="$OMZ_CUSTOM_DIR/themes"
 MY_THEME_BASE_URL="https://raw.github.com/albenik/shell_scripts/master/custom/themes"
 
+wget --no-check-certificate http://install.ohmyz.sh -O - | sh
+
 if [[ ! -d $OMZ_CUSTOM_DIR ]]
 then
   echo "oh-my-zsh folders not found"
